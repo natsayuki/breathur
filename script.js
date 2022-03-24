f72609511688aca957078be44335605e4f2f8c7b
natsayuki <the42ndturtle@gmail.com>
2022-03-24 02:00:40 -0400

    circle test

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const data = {
   wholeTime: 0,
   breathingIn: false,
   breathing: false,
+  landscape: window.width > window.height,
 }
 
 const map = (value, x1, y1, x2, y2) => (value - x1) * (y2 - x2) / (y1 - x1) + x2;
